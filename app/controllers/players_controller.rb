449f4b444a48437bbc157fca216c04b259b300aa
Adam Zerner <azerner3@gmail.com>
2019-12-16 13:54:55 -0800

    Fixes for Player CRUD

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -9,6 +9,7 @@ class PlayersController < ApplicationController
 
   def new
     @player = Player.new
+    @player.address = Address.new
   end
 
   def edit
@@ -18,7 +19,7 @@ class PlayersController < ApplicationController
   def create
     @player = Player.new(player_params)
 
-    if (@player.save && @player.create_address(address_params))
+    if @player.save
       redirect_to @player
     else
       render 'new'
@@ -28,7 +29,7 @@ class PlayersController < ApplicationController
   def update
     @player = Player.find(params[:id])
 
-    if (@player.update(player_params) && @player.address.update(address_params))
+    if @player.update(player_params)
       redirect_to @player
     else
       render 'edit'
@@ -46,18 +47,10 @@ class PlayersController < ApplicationController
     def player_params
       params_for_player = params
         .require(:player)
-        .permit(:first_name, :middle_name, :last_name, :height, :weight, :birthday, :high_school_team, :club_team, :email, :phone_number, :address, :notes)
-        .except(:address)
+        .permit(:first_name, :middle_name, :last_name, :height, :weight, :birthday, :high_school_team, :club_team, :email, :phone_number, :notes, address_attributes: [ :id, :line_one, :line_two, :city, :state, :zip ])
       params_for_player["high_school_team"] = nil
       params_for_player["club_team"] = nil
 
       return params_for_player
-    end
-
-    def address_params
-      params
-        .require(:player)
-        .require(:address)
-        .permit(:line_one, :line_two, :city, :state, :zip)
     end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,6 @@ class Player < ApplicationRecord
   has_one :address, dependent: :destroy
   belongs_to :high_school_team, optional: true
   belongs_to :club_team, optional: true
+
+  accepts_nested_attributes_for :address, allow_destroy: true
 end
