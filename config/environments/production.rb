ff2c12d65bd7f2f1759804b70ad55bc93c54067f
Adam Zerner <azerner3@gmail.com>
2020-01-19 13:37:24 -0800

    User accounts with devise initial setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "scout_db_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'http://scout-db.herokuapp.com/', port: 80 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
