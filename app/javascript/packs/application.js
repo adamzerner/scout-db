7fa622b41b7b136c2ab8ebf75acfa6453a446a67
Adam Zerner <azerner3@gmail.com>
2020-01-20 12:10:44 -0800

    Use webpacker instead of asset pipeline for CSS and images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import "../stylesheets/application"
-import "bootstrap/scss/bootstrap.scss"
 
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -20,6 +19,6 @@ require("@rails/actiontext")
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
