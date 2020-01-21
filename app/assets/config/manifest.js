c5e3c87babb82538a3ad20a05c2a7b595c4bcc18
Adam Zerner <azerner3@gmail.com>
2020-01-21 11:22:25 -0800

    Add app/assets/config/manifest.js back in because of bootsnap dependency

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+// I only have this file because bootsnap requires it. Otherwise I'd like to remove app/assets given that I'm not using the asset pipeline anymore. See https://github.com/Shopify/bootsnap/issues/291.
+
+//= link_tree ../images
+//= link_directory ../stylesheets .css
