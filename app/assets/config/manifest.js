e9d552942070a6ea2530f3a4eaaf1bdd5ac5ced4
Adam Zerner <adamzerner@protonmail.com>
2020-01-23 13:10:21 -0800

    Remove unneeded code from app/assets/config/manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,1 @@
 // I only have this file because some railtie stuff seems to require it. I'd like to get rid of it given that I'm not using webpacker instead of the asset pipeline.
-
-//= link_tree ../images
-//= link_directory ../stylesheets .css
